6d591de38924ebb4e9e5cc2e4d6f742fb03b32cb
Lena Hou <lenahou2013@gmail.com>
2021-11-29 10:59:05 -0500

    commit tree 6f0b8bba605830afee228d12467d5162d749ebb5

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, { unstable_renderSubtreeIntoContainer } from 'react-dom';
+// Step 1. Import react-router functions
+import { BrowserRouter as Router, Route } from "react-router-dom" 
+import Home from './Home'
+import Login from './Login'
+import About from './About'
+import Navbar from './Navbar'
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
+
 
 ReactDOM.render(
-  <Home />,
+  <Router>
+    <div>
+      <Navbar />
+  <Route exact path="/" component={Home} />
+  <Route exact path="/about" component={About} />
+  <Route exact path="/login" component={Login} />
+  </div>
+  </Router>,
   document.getElementById('root')
 );
